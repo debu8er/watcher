12acb9d59de28d6de1684a7e4ad278e782c87ef8
debu8er <121763963+debu8er@users.noreply.github.com>
2024-05-06 23:47:08 +0330

    Update mymongo.py

--- mymongo.py
+++ mymongo.py
@@ -1,60 +1,65 @@
+
 import pymongo
 import json
 from bson import json_util
 
-def initialize_mongo_collection(domain, mongodb_uri):
-    client = pymongo.MongoClient(mongodb_uri)
-    db = client["mydatabase"]
-    collection = db[domain]
-    return collection
+class MongoOperations:
+    def __init__(self, domain):
+        self.mongodb_uri = "mongodb://localhost:27017"
+        self.client = pymongo.MongoClient(self.mongodb_uri)
+        self.db = self.client["mydatabase"]
+        self.collection = self.db[domain]
+        self.collection2 = self.db["domains"]
 
-def print_document(document):
-    formatted_document = json.dumps(document, indent=2, default=json_util.default)
-    print(formatted_document)
+    def print_document(self, document, only_sub=False):
+        if only_sub:
+            print(document.get("sub", "No subdomain found"))
+        else:
+            formatted_document = json.dumps(document, indent=2, default=json_util.default)
+            print(formatted_document)
 
-def get_status_changed(collection):
-    results = collection.find({"status_changed": {"$ne": False}})
-    for document in results:
-        print_document(document)
+    def get_status_changed(self, only_sub):
+        results = self.collection.find({"status_changed": {"$ne": False}})
+        for document in results:
+            self.print_document(document, only_sub)
 
-def get_tech_changed(collection):
-    results = collection.find({"tech_changed": {"$ne": False}})
-    for document in results:
-        print_document(document)
+    def get_tech_changed(self, only_sub):
+        results = self.collection.find({"tech_changed": {"$ne": False}})
+        for document in results:
+            self.print_document(document, only_sub)
 
-def get_fresh(collection):
-    query = {"fresh": {"$ne": False}}
-    results = collection.find(query)
-    for document in results:
-        print_document(document)
+    def get_fresh(self, only_sub):
+        results = self.collection.find({"fresh": {"$ne": False}})
+        for document in results:
+            self.print_document(document, only_sub)
 
-def get_all_sub(collection):
-    results = collection.find()
-    for document in results:
-        print(document["sub"])
+    def get_all_sub(self):
+        results = self.collection.find()
+        for document in results:
+            print(document.get("sub", "No subdomain found"))
 
-def add_domain(collection, domain):
-    mydoc = {"domain": domain}
-    collection.insert_one(mydoc)
+    def add_domain(self):
+        mydoc = {"domain": self.collection.name}
+        self.collection2.insert_one(mydoc)
 
-def remove_domain(collection, domain):
-    mydoc = {"domain": domain}
-    collection.delete_one(mydoc)
+    def remove_domain(self):
+        mydoc = {"domain": self.collection.name}
+        self.collection2.delete_one(mydoc)
 
-def get_full(collection):
-    documents = collection.find()
-    for document in documents:
-        print_document(document)
+    def get_full(self):
+        documents = self.collection.find()
+        for document in documents:
+            self.print_document(document)
 
-def filter_status(collection, status):
-    query = {"status": {"$eq": status}}
-    documents = collection.find(query)
-    for document in documents:
-        print_document(document)
+    def filter_status(self, status, only_sub):
+        query = {"status": int(status)} if status and status is not True else {"status": {"$ne": None}}
+        documents = self.collection.find(query)
+        for document in documents:
+            self.print_document(document, only_sub)
 
-def filter_tech(collection, tech):
-    query = {"status": {"$eq": tech}}
-    documents = collection.find()
-    for document in documents:
-        if document["tech"] != None and tech in document["tech"]:
-            print_document(document)
+    def filter_tech(self, tech, only_sub):
+        regex_pattern = f"(?i){tech}" if tech and tech is not True else {"tech": {"$ne": None}}
+        query = {"tech": {"$regex": regex_pattern}} if tech and tech is not True else {"tech": {"$ne": None}}
+        documents = self.collection.find(query)
+        for document in documents:
+            self.print_document(document, only_sub)
